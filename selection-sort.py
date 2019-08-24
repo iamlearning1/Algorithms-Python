837a6cd7edfe899224add07d3b8730a244726349
Deepak Kumar <d1612k@gmail.com>
2019-08-24 19:01:37 +0530

    selection sort

---
+++ selection-sort.py
@@ -0,0 +1,15 @@
+def selection_sort(arr):
+    i = 0
+    while i < len(arr):
+        j = i + 1
+        while j < len(arr):
+            if (arr[i] > arr[j]):
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            j += 1
+        i += 1
+    return arr
+
+
+print(selection_sort([31, 17, 1, 7, 34, 13]))

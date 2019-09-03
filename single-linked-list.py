7e16611704b94a29aad4458d77702c2fa6f7fb2a
Deepak Kumar <d1612k@gmail.com>
2019-09-03 21:22:52 +0530

    Linked list pop

--- single-linked-list.py
+++ single-linked-list.py
@@ -18,7 +18,7 @@ class SingleLinkedList:
     def __repr__(self):
         return f'Head: {self.head}\nTail: {self.tail}\nLength: {self.length}'
 
-    def len(self):
+    def __len__(self):
         return self.length
 
     def append(self, val):
@@ -32,9 +32,30 @@ class SingleLinkedList:
         self.length += 1
         return self
 
+    def pop(self):
+        if not self.head:
+            return None
+
+        current = self.head
+        new_tail = current
+        while current.next:
+            new_tail = current
+            current = current.next
+
+        self.tail = new_tail
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length is 0:
+            self.head = None
+            self.tail = None
+
+        return self
+
 
 linked_list = SingleLinkedList()
 linked_list.append('Hello')
 linked_list.append('World')
 linked_list.append('!')
+linked_list.pop()
 print(linked_list)
